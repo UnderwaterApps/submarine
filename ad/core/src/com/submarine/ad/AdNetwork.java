955542fb02ecfc8efdabe3309b331d303ae35364
Sargis Sargsyan <sargsyan.sargis.89@gmail.com>
2015-02-06 11:49:03 +0400

    first commit

---
+++ ad/core/src/com/submarine/ad/AdNetwork.java
@@ -0,0 +1,12 @@
+package com.submarine.ad;
+
+/**
+ * Created by sargis on 1/30/15.
+ */
+public interface AdNetwork {
+    void showBanner();
+
+    void hideBanner();
+
+    void showInterstitial();
+}

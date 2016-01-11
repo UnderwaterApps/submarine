78f33a616830cd15cf0851f83cd31fc78ef7f21e
mariamKh <mariam@underwaterapps.com>
2016-01-11 16:15:04 +0400

    refactoring from adMob to ad

---
+++ ad/ios/src/com/submarine/ad/IOSTempAdMobNetwork.java
@@ -0,0 +1,21 @@
+package com.submarine.ad;
+
+/**
+ * Created by sargis on 2/6/15.
+ */
+public class IOSTempAdMobNetwork implements AdNetwork {
+    @Override
+    public void showBanner() {
+
+    }
+
+    @Override
+    public void hideBanner() {
+
+    }
+
+    @Override
+    public void showInterstitial() {
+
+    }
+}
